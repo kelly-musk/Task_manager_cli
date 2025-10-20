4e931797a997c00b24604b5dd001a65664c3cba1
kelly-musk <ogunjioforkelechi@gmail.com>
2025-10-20 13:43:09 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
+use std::path::Path;
+
+const FILE_PATH: &str = "task.json";
+fn main() {
+    println!{"he"}
+}
